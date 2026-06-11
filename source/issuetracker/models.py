02184c1e99a2f53f2d62a3007d1616a235d88bef
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-11 13:11:40 +0600

    Modified in models.py on_delete Protect to Restrict

--- source/issuetracker/models.py
+++ source/issuetracker/models.py
@@ -9,7 +9,7 @@ class Type(models.Model):
 class Task(models.Model):
     summary = models.CharField(null=False, blank=False, max_length=500, verbose_name='Краткое описание')
     description = models.CharField(null=True, blank=True, max_length=500, verbose_name='Подробное описание')
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name='Статус')
-    type = models.ForeignKey(Type, on_delete=models.PROTECT, verbose_name='Тип')
+    status = models.ForeignKey(Status, on_delete=models.RESTRICT, verbose_name='Статус')
+    type = models.ForeignKey(Type, on_delete=models.RESTRICT, verbose_name='Тип')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления')

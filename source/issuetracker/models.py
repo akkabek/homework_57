86cdd976dfb7d0c22a2a2aa34dfa1e8eb90c2a45
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-11 13:10:45 +0600

    Added models: Status, Type, Task

--- source/issuetracker/models.py
+++ source/issuetracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50, verbose_name='Название')
+
+class Type(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50, verbose_name='Название')
+
+class Task(models.Model):
+    summary = models.CharField(null=False, blank=False, max_length=500, verbose_name='Краткое описание')
+    description = models.CharField(null=True, blank=True, max_length=500, verbose_name='Подробное описание')
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey(Type, on_delete=models.PROTECT, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления')

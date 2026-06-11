ed01733c884f68c9f56f4bc8e506cc3abbfd355f
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-11 16:59:08 +0600

    in models description changed to Textfield

--- source/issuetracker/models.py
+++ source/issuetracker/models.py
@@ -14,7 +14,7 @@ class Type(models.Model):
 
 class Task(models.Model):
     summary = models.CharField(null=False, blank=False, max_length=500, verbose_name='Краткое описание')
-    description = models.CharField(null=True, blank=True, max_length=500, verbose_name='Подробное описание')
+    description = models.TextField(null=True, blank=True, max_length=500, verbose_name='Подробное описание')
     status = models.ForeignKey(Status, on_delete=models.RESTRICT, verbose_name='Статус')
     type = models.ForeignKey(Type, on_delete=models.RESTRICT, verbose_name='Тип')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')

09fd28b6eb42cb45a0e23d400d8924a5c09643b6
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-11 17:20:30 +0600

    Type in TaskForm modified to many-to-many

--- source/issuetracker/admin.py
+++ source/issuetracker/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 from issuetracker.models import Type, Task, Status
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id','summary', 'status', 'type','created_at','updated_at']
+    list_display = ['id','summary', 'status', 'get_types','created_at','updated_at']
     list_filter = ['status','type', 'created_at']
     search_fields = ['summary']
     fields = ['summary','description' ,'status', 'type','created_at','updated_at']
     readonly_fields = ['created_at','updated_at']
+
+    def get_types(self, obj):
+        return ', '.join(t.name for t in obj.type.all())
+
+    get_types.short_description = 'Тип'
 
 class StatusAdmin(admin.ModelAdmin):
     list_display = ['id','name']
--- source/issuetracker/models.py
+++ source/issuetracker/models.py
@@ -16,6 +16,6 @@ class Task(models.Model):
     summary = models.CharField(null=False, blank=False, max_length=500, verbose_name='Краткое описание')
     description = models.TextField(null=True, blank=True, max_length=500, verbose_name='Подробное описание')
     status = models.ForeignKey(Status, on_delete=models.RESTRICT, verbose_name='Статус')
-    type = models.ForeignKey(Type, on_delete=models.RESTRICT, verbose_name='Тип')
+    type = models.ManyToManyField(Type, verbose_name='Тип')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления')

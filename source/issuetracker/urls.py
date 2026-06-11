db0a7da07da7069b7ec6c41ca3f6c74e1b820eb8
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-11 15:47:49 +0600

    added urls and views for index and detail view

--- source/issuetracker/urls.py
+++ source/issuetracker/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from issuetracker.views import IndexView, DetailView
 
 urlpatterns = [
-
+    path('', IndexView.as_view(), name='index'),
+    path('tasks/', IndexView.as_view(), name='index'),
+    path('tasks/<int:pk>', DetailView.as_view(), name='detail'),
 ]
--- source/issuetracker/views.py
+++ source/issuetracker/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from curses.ascii import controlnames
 
-# Create your views here.
+from django.views.generic import TemplateView
+from issuetracker.models import Task
+from django.shortcuts import get_object_or_404, redirect
+
+class IndexView(TemplateView):
+    template_name = 'webapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['tasks'] = Task.objects.all()
+
+        return context
+
+class DetailView(TemplateView):
+    template_name = 'webapp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=self.kwargs['pk'])
+        return context
+

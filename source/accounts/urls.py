6a12fcf9a9b8d6c2c526fbd17c900dcd09d32ae1
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-25 18:21:24 +0600

    Logout переписан как класс, обновлен url

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
-
-from accounts.views import logout_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]

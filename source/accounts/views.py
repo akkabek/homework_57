11c576e772fd63610318ee134568ec0c26ab51ae
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-25 16:32:56 +0600

    Добавлены вьюшки для логина и логаута

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def login_view(request):
     context = {}
@@ -9,7 +9,11 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('article_index')
+            return redirect('issuetracker:projects_list')
         else:
             context['has_error'] = True
-    return render(request, 'login.html', context=context)
+    return render(request, 'accounts/login.html', context=context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('issuetracker:projects_list')

3323d551d4683a3504c60f420fc35063744d3a5d
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-29 16:54:54 +0600

    Добавлена вьюшка и url Регистрации

---
+++ source/accounts/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import MyUserCreationForm
+
+User = get_user_model()
+
+
+class RegisterView(CreateView):
+    form_class = MyUserCreationForm
+    template_name = "accounts/registration.html"
+    model = User
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        redirect_url = reverse("issuetracker:projects_list")
+
+        if self.request.GET.get("next"):
+            redirect_url = self.request.GET.get("next")
+
+        if self.request.POST.get("next"):
+            redirect_url = self.request.POST.get("next")
+        return redirect_url

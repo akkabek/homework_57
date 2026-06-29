2dbf5f8c602596a3bbeaf7d8c6c539151d6043c6
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-29 17:32:22 +0600

    Модели Type и Status добавлены в init

--- source/issuetracker/models/project.py
+++ source/issuetracker/models/project.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
 
 class Project(models.Model):
     begin_date = models.DateField(null=False, blank=False, verbose_name='Дата начала')
     end_date = models.DateField(null=True, blank=True, verbose_name='Дата окончания')
     title = models.CharField(null=False, blank=False, max_length=100, verbose_name='Название')
     description = models.TextField(null=False, blank=False, max_length=500, verbose_name='Описание')
+
+    users = models.ManyToManyField(
+        get_user_model(),
+        related_name='projects',
+        blank=True,
+        verbose_name='Участники'
+    )
 
     def __str__(self):
         return self.title
